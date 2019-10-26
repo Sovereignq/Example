7298e29d99cb0612326d3629745b9f70945238bc
Sovereign <boyko.rusya.03@gmail.com>
2019-10-26 10:20:11 +0300

    second

--- app/src/main/java/com/example/uml/fragment/SignupFragment.java
+++ app/src/main/java/com/example/uml/fragment/SignupFragment.java
@@ -1,5 +1,6 @@
 package com.example.uml.fragment;
 
+import android.graphics.Color;
 import android.widget.Button;
 import android.widget.EditText;
 import com.example.uml.R;
@@ -15,16 +16,18 @@ import org.androidannotations.annotations.ViewById;
 import java.util.Objects;
 import rx.Observable;
 import rx.functions.Action1;
+import rx.functions.Action2;
 import rx.functions.Func2;
+import rx.functions.Func3;
 
 @EFragment(R.layout.fragment_signup)
-public class SignupFragment extends BaseFragment {
+class SignupFragment extends BaseFragment {
 
     @ViewById(R.id.btnLogin)
     Button btnLogin;
 
     @AfterViews
-    public void mainWork() {
+    void mainWork() {
         EditText inputEmail = Objects.requireNonNull(getActivity()).findViewById(R.id.inputEmail);
         EditText inputPassword = Objects.requireNonNull(getActivity()).findViewById(R.id.inputPassword);
         EditText inputConfirmPassword = Objects.requireNonNull(getActivity()).findViewById(R.id.inputConfirmPassword);
@@ -33,23 +36,24 @@ public class SignupFragment extends BaseFragment {
 
         Observable<String> emailObservable = RxEditText.getTextWatcherObservable(inputEmail);
         Observable<String> passwordConfirmObservable = RxEditText.getTextWatcherObservable(inputConfirmPassword);
+        Observable<String> passwordObservable = RxEditText.getTextWatcherObservable(inputPassword);
 
-        Observable.combineLatest(emailObservable, passwordConfirmObservable, new Func2<String, String, Boolean>() {
-
-            @Override
-            public Boolean call(String s, String s2) {
-                if(s.isEmpty() || s2.isEmpty())
-                    return false;
-                else
-                    return true;
-            }
+        Observable.combineLatest(emailObservable, passwordObservable, passwordConfirmObservable, (s, s2, s3) -> {
+            if(s.isEmpty() || s2.isEmpty() || s3.isEmpty() || !s2.equals(s3))
+                return false;
+            else
+                return true;
         }).subscribe(new Action1<Boolean>() {
             @Override
             public void call(Boolean aBoolean) {
-                buttonSignup.setEnabled(aBoolean);
+                if (!aBoolean) {
+                    buttonSignup.setEnabled(aBoolean);
+                    buttonSignup.setBackgroundColor(Color.DKGRAY);
+                }
             }
         });
     }
+
 
     @Click
     void btnLogin() {
